3b2759a9a36b1d1c3afea6b22594f6ccf4ae1875
asharyapandey <asharyap7@gmail.com>
2020-12-04 18:46:10 +0545

    added some functions

--- app/src/main/java/com/asharya/calculator/MainActivity.kt
+++ app/src/main/java/com/asharya/calculator/MainActivity.kt
@@ -9,6 +9,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var tvEquation: TextView
     private lateinit var tvAnswer: TextView
+    private var numberOne = ""
+    private var expression = ""
+    private var numberTwo = ""
+    private var finalExpression = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,5 +28,70 @@ class MainActivity : AppCompatActivity() {
         val btnSeven: Button = findViewById(R.id.btnSeven)
         val btnEight: Button = findViewById(R.id.btnEight)
         val btnNine: Button = findViewById(R.id.btnNine)
+        val btnAdd: Button = findViewById(R.id.btnAdd)
+        val btnMultiply: Button = findViewById(R.id.btnMultiply)
+        val btnDivide: Button = findViewById(R.id.btnDivide)
+        val btnSubtract: Button = findViewById(R.id.btnSubtract)
+        val btnEquals: Button = findViewById(R.id.btnEquals)
+        val btnClear: Button = findViewById(R.id.btnClear)
+
+        val buttons = mutableListOf<Button>(btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine, btnAdd, btnSubtract, btnDivide, btnMultiply)
+
+        for(button in buttons) {
+            button.setOnClickListener {
+                set(button.text.toString())
+            }
+        }
+
+        btnEquals.setOnClickListener {
+            calculate()
+        }
+
+        btnClear.setOnClickListener {
+            reset()
+        }
+    }
+
+    private fun reset() {
+        finalExpression  = ""
+        expression = ""
+        numberTwo = ""
+        numberOne = ""
+        tvEquation.text = "0"
+        tvAnswer.text = "0"
+    }
+
+    private fun set(text: String) {
+        if (finalExpression != "") {
+            reset()
+        }
+        when(text) {
+            "+", "-", "*","/", "%" -> {
+                expression = text
+                tvAnswer.text = "$numberOne $expression"
+            }
+            else -> {
+               if (expression == "") {
+                   numberOne += text
+                   tvAnswer.text = numberOne
+               } else {
+                   numberTwo += text
+                   tvAnswer.text = "$numberOne $expression $numberTwo"
+               }
+
+            }
+        }
+
+    }
+    private fun calculate() {
+        tvEquation.text = finalExpression
+        val result = when(expression) {
+            "+" -> numberOne.toInt() + numberTwo.toInt()
+            "-" -> numberOne.toInt() - numberTwo.toInt()
+            "*" -> numberOne.toInt() * numberTwo.toInt()
+            "%" -> numberOne.toInt() / numberTwo.toInt()
+            else -> ""
+        }
+        tvAnswer.text = result.toString()
     }
 }
