a87fbd156b2dddba919144698b6d51993dd90447
asharyapandey <asharyap7@gmail.com>
2020-12-04 12:58:48 +0545

    binding

--- app/src/main/java/com/asharya/calculator/MainActivity.kt
+++ app/src/main/java/com/asharya/calculator/MainActivity.kt
@@ -2,10 +2,27 @@ package com.asharya.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvEquation: TextView
+    private lateinit var tvAnswer: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvAnswer= findViewById(R.id.tvAnswer)
+        tvEquation= findViewById(R.id.tvEquation)
+        val btnOne : Button = findViewById(R.id.btnOne)
+        val btnTwo: Button = findViewById(R.id.btnTwo)
+        val btnThree: Button = findViewById(R.id.btnThree)
+        val btnFour: Button = findViewById(R.id.btnFour)
+        val btnFive: Button = findViewById(R.id.btnFive)
+        val btnSix: Button = findViewById(R.id.btnSix)
+        val btnSeven: Button = findViewById(R.id.btnSeven)
+        val btnEight: Button = findViewById(R.id.btnEight)
+        val btnNine: Button = findViewById(R.id.btnNine)
     }
 }
